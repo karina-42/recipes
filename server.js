59a40e70686eb82b7b88385f2b2d5bc0946877aa
Angela Vegega <angela.vegega@gmail.com>
2022-08-20 15:48:38 +0900

    added folders and files necessary for mvc

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose') 
-require('dotenv').config()
+const connectDB = require('./config/database')
+const homeRoutes = require('./routes/home')
+
+require('dotenv').config({path: './config/.env'})
 const PORT = 8000
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/', homeRoutes)
 
 app.listen(PORT, () => {
   console.log(`Go catch the server at port ${PORT}`);
